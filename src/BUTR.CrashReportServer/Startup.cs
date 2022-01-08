d10b2354ea93442a51cc1046338e4c5a2be2d561
Aragas <personal@aragas.org>
2022-01-09 01:37:29 +0300

    Context fix
    Formatting

--- src/BUTR.CrashReportServer/Startup.cs
+++ src/BUTR.CrashReportServer/Startup.cs
@@ -3,7 +3,6 @@
 using BUTR.CrashReportServer.Options;
 using BUTR.CrashReportServer.Services;
 
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
