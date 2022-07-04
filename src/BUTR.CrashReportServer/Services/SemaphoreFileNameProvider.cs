f3219656efad350193b4041858df3340e62c16bf
Aragas <personal@aragas.org>
2022-07-04 22:31:32 +0300

    Faster hex generator

--- src/BUTR.CrashReportServer/Services/SemaphoreFileNameProvider.cs
+++ src/BUTR.CrashReportServer/Services/SemaphoreFileNameProvider.cs
@@ -3,6 +3,8 @@
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
+using Numeral;
+
 using System;
 using System.IO;
 using System.Threading;
@@ -29,18 +31,30 @@ namespace BUTR.CrashReportServer.Services
             _options = options;
         }
 
+        private string? GetHex(CancellationToken ct)
+        {
+            Span<byte> buffer = stackalloc byte[5];
+            Span<char> buffer2 = stackalloc char[10];
+            while (!ct.IsCancellationRequested)
+            {
+                _random.NextBytes(buffer);
+                HexConverter.GetChars(buffer, buffer2);
+                for (var i = 0; i < buffer2.Length; i++)
+                    buffer2[i] = char.ToUpper(buffer2[i]);
+                
+                var filePath = Path.GetFullPath(Path.Combine(_options.Path ?? string.Empty, $"{buffer2}.html"));
+                if (!File.Exists(filePath))
+                    return filePath;
+            }
+            return null;
+        }
+        
         public async Task<string?> GenerateUniqueFilePath(CancellationToken ct)
         {
             try
             {
                 await _lock.WaitAsync(ct);
-                while (!ct.IsCancellationRequested)
-                {
-                    var filePath = Path.GetFullPath(Path.Combine(_options.Path ?? string.Empty, $"{_random.Next():X2}.html"));
-                    if (!File.Exists(filePath))
-                        return filePath;
-                }
-                return null;
+                return GetHex(ct);
             }
             finally
             {
